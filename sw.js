3beced0cf13f181daeeae9689555963c9e6cda91
rosas16jonathan-hue <rosas16jonathan@gmail.com>
2026-03-30 12:29:25 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-
-const CACHE_NAME = 'ecozen-v1';
-const ASSETS = [
+const STATIC_CACHE = 'ecozen-static-v3';
+const RUNTIME_CACHE = 'ecozen-runtime-v3';
+const APP_SHELL = [
   './',
   './index.html',
   './app.js',
@@ -9,22 +9,39 @@ const ASSETS = [
   './icon-512.png'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(STATIC_CACHE).then((cache) => cache.addAll(APP_SHELL)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => ![STATIC_CACHE, RUNTIME_CACHE].includes(key)).map((key) => caches.delete(key))
     ))
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
-  );
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+
+  if (url.origin === self.location.origin) {
+    event.respondWith(
+      caches.match(event.request).then((cached) => {
+        const network = fetch(event.request)
+          .then((response) => {
+            const clone = response.clone();
+            caches.open(RUNTIME_CACHE).then((cache) => cache.put(event.request, clone));
+            return response;
+          })
+          .catch(() => cached || caches.match('./index.html'));
+        return cached || network;
+      })
+    );
+    return;
+  }
+
+  event.respondWith(fetch(event.request).catch(() => caches.match('./index.html')));
 });
